a8256f3ca0fd165e6327a8d4f8455fcaadabfc28
Egor-arkharov <egorarkharov@gmail.com>
2020-08-26 21:46:43 +0300

    Начало js, верстка сложных компонентов, форм

--- source/js/header.js
+++ source/js/header.js
@@ -4,15 +4,13 @@
   var header = document.querySelector('.header');
   var toggle = header.querySelector('.header__toggle-nav');
 
+  header.classList.remove('header--nojs');
+
   toggle.addEventListener('click', function () {
     if (header.classList.contains('header--opened')) {
       header.classList.remove('header--opened');
-      header.classList.add('header--closed');
     } else {
-      header.classList.remove('header--closed');
       header.classList.add('header--opened');
     }
-
-
   })
 })();
