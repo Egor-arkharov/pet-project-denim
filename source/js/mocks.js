f8ff69a8df547a8ad145d87d650969340a7ecba7
Egor-arkharov <egorarkharov@gmail.com>
2020-08-27 23:21:58 +0300

    Перед созданием моков и темплейтов

---
+++ source/js/mocks.js
@@ -0,0 +1,50 @@
+'use strict';
+
+(function () {
+  var BESTSELLERS = [
+    {
+      category: 'Jacket',
+      price: 120,
+      photo: {
+        mob: 'img/mob/bestsellers_jacket_120.png',
+        tab: 'img/tab/bestsellers_jacket_120.png',
+        desk: 'img/desk/bestsellers_jacket_120.png'
+      },
+      alt: 'Man with lightblue jacket'
+    },
+    {
+      category: 'Overalls',
+      price: 140,
+      photo: {
+        mob: 'img/mob/bestsellers_overalls_140.png',
+        tab: 'img/tab/bestsellers_overalls_140.png',
+        desk: 'img/desk/bestsellers_overalls_140.png'
+      },
+      alt: 'Girl with overalls in front of building'
+    },
+    {
+      category: 'Jacket',
+      price: 90,
+      photo: {
+        mob: 'img/mob/bestsellers_jacket_90.png',
+        tab: 'img/tab/bestsellers_jacket_90.png',
+        desk: 'img/desk/bestsellers_jacket_90.png',
+      },
+      alt: 'Girl with jacket from back'
+    },
+    {
+      category: 'Overalls',
+      price: 160,
+      photo: {
+        mob: 'img/mob/bestsellers_overalls_160.png',
+        tab: 'img/tab/bestsellers_overalls_160.png',
+        desk: 'img/desk/bestsellers_overalls_160.png'
+      },
+      alt: 'Girl with overalls looking away'
+    }
+  ];
+
+
+
+
+})();
